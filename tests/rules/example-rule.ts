262d4a1a15c3a7835d4b432b62a6f51bdd7cb1f8
Anton Krylov <adkrylov@avito.ru>
2023-03-16 00:01:39 +0600

    update tests

--- tests/rules/example-rule.ts
+++ tests/rules/example-rule.ts
@@ -1,11 +1,18 @@
-import { TSESLint } from '@typescript-eslint/experimental-utils';
-import rule from '../../src/rules/example-rule';
+import { RuleTester } from "eslint";
+import rule from "../../src/rules/example-rule";
 
-new TSESLint.RuleTester().run('example-rule', rule, {
-  valid: ['var foo = 1;'],
+const tester = new RuleTester({
+  parserOptions: {
+      ecmaVersion: 6,
+      sourceType: 'module'
+  }
+});
+
+tester.run('example-rule', rule, {
+  valid: ['const foo = 1;'],
   invalid: [
     {
-      code: 'var example = 1;',
+      code: 'const example = 1;',
       errors: [{ messageId: 'disallowExample' }],
     },
   ],
